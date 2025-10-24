d2ba3b270c932b31a5f0836cf4f45f8b3e9d773c
Аспарух Виткинов <vitkinov90@gmail.com>
2025-10-24 10:18:27 +0300

    update member info

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import ScrollToTop from "@/components/ScrollToTop";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -39,6 +40,7 @@ export default function RootLayout({
         <Header />
         <main className="grow">
           {children}
+          <Analytics />
         </main>
         <Footer />
         <ScrollToTop />
